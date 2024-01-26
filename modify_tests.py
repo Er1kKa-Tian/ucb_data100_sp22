4d4a431bfa86b48e38bb7762e28831ec2f2507b7
Er1kKa Tian <er1kka.tian@outlook.com>
2024-01-26 20:12:06 +0800

    Reformat test modifying script using black

--- modify_tests.py
+++ modify_tests.py
@@ -1,14 +1,17 @@
 import os
 
-def main(): 
-    for folder_path, _, files in os.walk(os.getcwd()):
 
-        py_test_files = [file for file in files if folder_path.endswith("tests") and file.endswith(".py")]
+def main():
+    for folder_path, _, files in os.walk(os.getcwd()):
+        py_test_files = [
+            file
+            for file in files
+            if folder_path.endswith("tests") and file.endswith(".py")
+        ]
 
         for py_test_file in py_test_files:
-
             file_path = os.path.join(folder_path, py_test_file)
-            
+
             with open(file_path, "r") as file:
                 content = file.read()
 
@@ -18,7 +21,6 @@ def main():
                 file.write(new_content)
 
             print(f"Modified {file_path}")
-
 
 
 if __name__ == "__main__":
